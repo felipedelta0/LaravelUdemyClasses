f0cfacdd46909b267fc4b96a06f204dd2c739056
Luis Felipe Alcântara dos Santos <felipedelta0@gmail.com>
2019-09-10 19:45:31 -0300

    Aula 012 - Parâmetros das Rotas

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/teste', function () {
+    return "Hello, World!";
+});
+
+Route::get('/ola/{nome}/{sobrenome}', function ($nome, $sb) { // Parâmetro na Rota {} - Não necessário ser o mesmo nome da variável
+    echo "Olá! Seja bem vindo, {$nome} {$sb}!";
+});
